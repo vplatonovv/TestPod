12ddd33b140b5052f5ba831a11b6f7ce0a127954
Viacheslav Platonov <slava.p12@yandex.ru>
2022-02-01 16:58:48 +0300

    try to add difference kit as dependency

--- Example/BlinkingLabel/ViewController.swift
+++ Example/BlinkingLabel/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import BlinkingLabel
 
 class ViewController: UIViewController {
+    
+    var blinkingLabel: BlinkingLabel!
+    var isBlinking = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Setup the BlinkingLabel
+        blinkingLabel = BlinkingLabel(frame: CGRect(x: 20, y: 30, width: 200, height: 30))
+        blinkingLabel.text = "I blink!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+        
+        // Create UIButton to toogle the blinking
+        let toogleButton = UIButton(frame: CGRect(x: 20, y: 70, width: 150, height: 30))
+        toogleButton.setTitle(" Toogle Blinking ", for: .normal)
+        toogleButton.setTitleColor(UIColor.red, for: .normal)
+        toogleButton.isEnabled = true
+        toogleButton.layer.borderColor = UIColor.black.cgColor
+        toogleButton.layer.borderWidth = 2
+        toogleButton.layer.cornerRadius = 10
+        toogleButton.addTarget(self, action: #selector(toogleBlinking), for: .touchUpInside)
+        view.addSubview(toogleButton)
+    }
+    
+    @objc func toogleBlinking() {
+        if isBlinking {
+            blinkingLabel.stopBlinking()
+        } else {
+            blinkingLabel.startBlinking()
+        }
+        isBlinking.toggle()
     }
 
     override func didReceiveMemoryWarning() {
